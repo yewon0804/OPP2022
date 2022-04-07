7474eabd6f72414f63368f38a6d2e3d8b57a9c11
yewon0804 <ehdnjs0804@gmail.com>
2022-04-07 19:37:38 +0900

    Complete week 5 task
    5주차 과제(실습 5-2) 해결..
    테이블 레이아웃을 활용해 레이아웃을 구성한
    숫자 버튼이있는 계산기 프로그램

--- app/src/main/java/com/example/opp2022/MainActivity.java
+++ app/src/main/java/com/example/opp2022/MainActivity.java
@@ -1,14 +1,117 @@
+/* **************************************
+ * 프로그램명: 실습 5-2 테이블레이아웃 계산기 앱
+ * 작성자: 2020039035 김예원
+ * 작성일: 2022. 04. 07.
+ * 프로그램 설명: 테이블 레이아웃을 활용하여 숫자 버튼이 있는 계산기
+ * ***************************************/
+
 package com.example.opp2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMult, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
+    Button[] numButtons = new Button[10];
+    Integer[] numBtnIDs = {R.id.btnNum0, R.id.btnNum1, R.id.btnNum2, R.id.btnNum3, R.id.btnNum4,
+            R.id.btnNum5, R.id.btnNum6, R.id.btnNum7, R.id.btnNum8, R.id.btnNum9};
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("계산기 프로그램");
+
+        edit1 = (EditText) findViewById(R.id.edit1);
+        edit2 = (EditText) findViewById(R.id.edit2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+        textResult = (TextView) findViewById(R.id.textResult);
+
+        btnAdd.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View arg0, MotionEvent motionEvent) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                textResult.setText("계산 결과: " + result.toString());
+                return false;
+            }
+        });
+        btnSub.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                textResult.setText("계산 결과: " + result.toString());
+                return false;
+            }
+        });
+        btnMult.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) * Integer.parseInt(num2);
+                textResult.setText("계산 결과: " + result.toString());
+                return false;
+            }
+        });
+        btnDiv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) / Integer.parseInt(num2);
+                textResult.setText("계산 결과: " + result.toString());
+                return false;
+            }
+        });
+
+        for (i = 0; i < numBtnIDs.length; i++) {
+            numButtons[i] = (Button) findViewById(numBtnIDs[i]);
+        }
+
+        for (i = 0; i <numBtnIDs.length; i++) {
+            final int index; // 꼭 필요함!!
+            index = i;
+
+            numButtons[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (edit1.isFocused()) {
+                        num1 = edit1.getText().toString()
+                                + numButtons[index].getText().toString();
+                        edit1.setText(num1);
+                    }
+                    else if (edit2.isFocused()) {
+                        num2 = edit2.getText().toString()
+                                + numButtons[index].getText().toString();
+                        edit2.setText(num2);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "먼저 에디트텍스트를 선택하세요", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 }
