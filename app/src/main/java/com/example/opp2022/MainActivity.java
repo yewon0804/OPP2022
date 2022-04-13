59b6b648d707aeaf7ac219c1da7b78baa26d786d
yewon0804 <ehdnjs0804@gmail.com>
2022-04-13 15:37:04 +0900

    Complete week6 task(6-2)
    6주차 과제(실습6-2) 해결..
    에디트 텍스트에 URL을 입력하면 해당 링크로 이동하는 앱
    화면의 로고와 프로그램의 아이콘 변경

--- app/src/main/java/com/example/opp2022/MainActivity.java
+++ app/src/main/java/com/example/opp2022/MainActivity.java
@@ -1,14 +1,66 @@
+/* **************************************
+ * 프로그램명: 실습 6-2 간단 웹브라우저 만들기
+ * 작성자: 2020039035 김예원
+ * 작성일: 2022. 04. 13.
+ * 프로그램 설명:
+ - 에디트 텍스트에 URL을 입력하면 해당 링크로 이동하는 앱
+ - 화면의 로고와 프로그램의 아이콘 변경
+ * ***************************************/
+
 package com.example.opp2022;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.web);
+
+        edtUrl = (EditText) findViewById(R.id.edtUrl);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        web = (WebView) findViewById(R.id.webView1);
+
+        web.setWebViewClient(new CookWebViewClient());
+
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                web.loadUrl(edtUrl.getText().toString());
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                web.goBack();
+            }
+        });
+
+    }
+
+    class CookWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
     }
 }
