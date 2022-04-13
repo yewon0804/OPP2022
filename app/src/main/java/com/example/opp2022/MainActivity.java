ff9295d0b3a397bf210c4e0a6ad37d09f87ffe9f
yewon0804 <ehdnjs0804@gmail.com>
2022-04-13 15:15:05 +0900

    Complete week6 task(6-1)
    5주차 과제(실습6-1) 해결..
    크로노미터, 프레임 레이아웃, 캘린더 뷰, 타임 피커

--- app/src/main/java/com/example/opp2022/MainActivity.java
+++ app/src/main/java/com/example/opp2022/MainActivity.java
@@ -1,14 +1,112 @@
+/* **************************************
+ * 프로그램명: 실습 6-1
+ * 작성자: 2020039035 김예원
+ * 작성일: 2022. 04. 13.
+ * 프로그램 설명:
+ - 타이머 기능: <예약 시작>과 <예약 완료>를 클릭하면 크로노미터가 타이머로 동작함
+ - 날짜/시간 설정: <날짜 설정>, <시간 설정>을 클릭하면 예약 날짜와 시간을 변경 가능
+ - <예약 완료>를 클릭하면 설정한 날짜와 시간이 결정됨
+ * ***************************************/
+
 package com.example.opp2022;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
+    Chronometer chrono;
+    RadioButton rdoCal, rdoTime;
+    DatePicker dPicker;
+    TimePicker tPicker;
+    TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("시간 예약");
+
+        // 크로노미터
+        chrono = (Chronometer) findViewById(R.id.chronometer1);
+
+        // 라디오버튼 2개
+        rdoCal = (RadioButton) findViewById(R.id.rdoCal);
+        rdoTime = (RadioButton) findViewById(R.id.rdoTime);
+
+        // FrameLayout의 2개 위젯
+        dPicker = (DatePicker) findViewById(R.id.datePicker1);
+        tPicker = (TimePicker) findViewById(R.id.timePicker1);
+
+        // 텍스트뷰 중에서 연,월,일,시,분 숫자
+        tvYear = (TextView) findViewById(R.id.tvYear);
+        tvMonth = (TextView) findViewById(R.id.tvMonth);
+        tvDay = (TextView) findViewById(R.id.tvDay);
+        tvHour = (TextView) findViewById(R.id.tvHour);
+        tvMinute = (TextView) findViewById(R.id.tvMinute);
+
+        // 처음에는 2개를 안보이게 설정
+        rdoCal.setVisibility(View.INVISIBLE);
+        rdoTime.setVisibility(View.INVISIBLE);
+        tPicker.setVisibility(View.INVISIBLE);
+        dPicker.setVisibility(View.INVISIBLE);
+
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tPicker.setVisibility(View.INVISIBLE);
+                dPicker.setVisibility(View.VISIBLE);
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tPicker.setVisibility(View.VISIBLE);
+                dPicker.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // 크로노미터를 클릭하면..타이머 시작
+        chrono.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+                rdoCal.setVisibility(View.VISIBLE);
+                rdoTime.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // 크로노미터를 롱클릭하면..타이머 중지
+        tvYear.setOnLongClickListener(new View.OnLongClickListener() {
+            public boolean onLongClick(View v) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+
+                tvYear.setText(Integer.toString(dPicker.getYear()));
+                tvMonth.setText(Integer.toString(1 + dPicker.getMonth()));
+                tvDay.setText(Integer.toString(dPicker.getDayOfMonth()));
+
+                tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
+                tvMinute.setText(Integer.toString(tPicker.getCurrentMinute()));
+
+                rdoCal.setVisibility(View.INVISIBLE);
+                rdoTime.setVisibility(View.INVISIBLE);
+                tPicker.setVisibility(View.INVISIBLE);
+                dPicker.setVisibility(View.INVISIBLE);
+                return false;
+
+            }
+        });
+
     }
+
 }
